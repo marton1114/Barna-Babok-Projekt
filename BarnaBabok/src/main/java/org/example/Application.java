ccdd1691d8eb85ff1887debc28114a46acad644b
marton1114 <fleiszmarci@gmail.com>
2022-04-11 14:10:07 +0300

    merge commit

--- BarnaBabok/src/main/java/org/example/Application.java
+++ BarnaBabok/src/main/java/org/example/Application.java
@@ -29,7 +29,7 @@ public class Application {
         processor.setSocketType("LGA 1200");
         processor.setNumOfCores(16);
         processor.setPower(125);
-        processor.setPrice(610.99);
+        processor.setPrice(611.99);
 
         /** TEST DATA BEGIN **/
         /** Component 02 - TEST ONLY **/
