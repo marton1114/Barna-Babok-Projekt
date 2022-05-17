698cb8f6d5392af865a199f00008e9f70e116045
marton1114 <fleiszmarci@gmail.com>
2022-05-17 22:26:36 +0300

    merge utólagos ellenőrzése és javítása

--- BarnaBabok/src/main/java/hu/unideb/inf/control/FilterTools/FilterConditionStringGenerator.java
+++
@@ -1,9 +0,0 @@
-package hu.unideb.inf.control.FilterTools;
-
-import javafx.scene.control.Slider;
-
-public class FilterConditionStringGenerator {
-    public static String generateConditions(Slider MaxPriceSlider) {
-        return " where price <= " + MaxPriceSlider.getValue();
-    }
-}
