6d4efcdabb259bad621b028cdad81a633e43ccf3
Izayata <moonwolf99public@gmail.com>
2022-05-18 14:00:09 +0200

    Kész főmenü

--- BarnaBabok/src/main/java/hu/unideb/inf/control/FXMLMainMenuSceneController.java
+++ BarnaBabok/src/main/java/hu/unideb/inf/control/FXMLMainMenuSceneController.java
@@ -5,6 +5,7 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
@@ -19,11 +20,48 @@ public class FXMLMainMenuSceneController {
     private Pane mainMenuScenePane;
 
     @FXML
+    private Pane creditsPane;
+
+    @FXML
     void handleMakeAConfigButtonClicked(MouseEvent event) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/FXMLSearchPageScene.fxml"));
+            Parent root = loader.load();
+
+            FXMLSearchPageSceneController scene2Controller = loader.getController();
+            scene2Controller.setSelectedTabPaneTab(0);
+
             stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            Scene scene = new Scene(loader.load());
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void handleCreditsButtonClicked() {
+        creditsPane.setVisible(true);
+    }
+
+    @FXML
+    void handleCreditsBackButtonPressed(MouseEvent event) {
+        creditsPane.setVisible(false);
+    }
+
+    @FXML
+    void handleConfigsButtonClicked(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/FXMLSearchPageScene.fxml"));
+            Parent root = loader.load();
+
+            FXMLSearchPageSceneController scene2Controller = loader.getController();
+            scene2Controller.setSelectedTabPaneTab(2);
+
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
 
