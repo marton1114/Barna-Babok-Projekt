134a06322ec2190a35bf3e4e1b277ce38cb85581
Izayata <moonwolf99public@gmail.com>
2022-05-18 13:58:45 +0200

    További css + Kilépés gomb és Komponens keresés gomb működik

--- BarnaBabok/src/main/java/hu/unideb/inf/MainApp.java
+++ BarnaBabok/src/main/java/hu/unideb/inf/MainApp.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 
@@ -16,6 +19,22 @@ public class MainApp extends Application {
         stage.setTitle("Göter");
         stage.setScene(scene);
         stage.show();
+
+        stage.setOnCloseRequest(event -> {
+            event.consume();
+            logout(stage);
+        });
+    }
+
+    public static void logout(Stage stage) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Kilépés");
+        alert.setContentText("Biztos ki szeretnél lépni az alkalmazásból?");
+
+        if (alert.showAndWait().get() == ButtonType.OK) {
+            stage.close();
+            System.exit(0);
+        }
     }
 
     public static void main(String[] args) {
--- BarnaBabok/src/main/java/hu/unideb/inf/control/FXMLMainMenuSceneController.java
+++ BarnaBabok/src/main/java/hu/unideb/inf/control/FXMLMainMenuSceneController.java
@@ -1,4 +1,42 @@
 package hu.unideb.inf.control;
 
+import hu.unideb.inf.MainApp;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 public class FXMLMainMenuSceneController {
+    Stage stage;
+
+    @FXML
+    private Pane mainMenuScenePane;
+
+    @FXML
+    void handleMakeAConfigButtonClicked(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/FXMLSearchPageScene.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(loader.load());
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void handleExitButtonClicked(MouseEvent event) {
+        stage = (Stage) mainMenuScenePane.getScene().getWindow();
+        MainApp.logout(stage);
+    }
+
+
 }
