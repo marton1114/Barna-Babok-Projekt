5e9fcced2625f5f12976f7326eff41d403f168cb
unknown <bihonrobert@gmail.com>
2022-05-19 15:56:45 +0200

    css 05 commit final

---
+++ BarnaBabok/src/main/java/hu/unideb/inf/model/components/JpaMemoryDAO.java
@@ -0,0 +1,49 @@
+package hu.unideb.inf.model.components;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class JpaMemoryDAO implements MemoryDAO{
+    final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("br.com.fredericci.pu");
+    final EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    @Override
+    public void saveMemory(Memory m) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(m);
+        entityManager.getTransaction().commit();
+    }
+
+    @Override
+    public void deleteMemory(Memory m) {
+        entityManager.getTransaction().begin();
+        entityManager.remove(m);
+        entityManager.getTransaction().commit();
+    }
+
+    @Override
+    public void updateMemory(Memory m) {
+        saveMemory(m);
+    }
+
+    @Override
+    public List<Memory> getMemories(String conditions) {
+        TypedQuery<Memory> query = entityManager.createQuery(
+                "SELECT a FROM Memory a" + conditions, Memory.class);
+
+        List<Memory> memories = query.getResultList();
+
+        // List<Componens> componensList = new ArrayList<>(memories);
+
+        return memories;
+    }
+
+    @Override
+    public void close() throws Exception {
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+}
---
+++ BarnaBabok/src/main/java/hu/unideb/inf/model/components/PowerSupply.java
@@ -0,0 +1,86 @@
+package hu.unideb.inf.model.components;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class PowerSupply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    protected String brand;
+    protected String series;
+    protected String model;
+    private int power;
+    private String energyEfficiency;
+    protected double price;
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getEnergyEfficiency() {
+        return energyEfficiency;
+    }
+
+    public void setEnergyEfficiency(String energyEfficiency) {
+        this.energyEfficiency = energyEfficiency;
+    }
+
+    @Override
+    public String toString() {
+        return "Gyártó: " + brand + '\n' +
+                "Sorozat: " + series + '\n' +
+                "Modell: " + model + '\n' +
+                "Effektivitás: " + energyEfficiency + '\n' +
+                "Teljesítmény: " + power + '\n' +
+                "Ár: " + price;
+    }
+}
