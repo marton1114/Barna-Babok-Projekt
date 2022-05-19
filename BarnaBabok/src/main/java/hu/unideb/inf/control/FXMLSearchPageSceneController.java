525973274e2c29b322d3311b42514c05c78b29c7
Izayata <moonwolf99public@gmail.com>
2022-05-19 09:52:33 +0200

    Átméretezés letiltása és 'komponens hozzáadása gomb' hibakezelések

--- BarnaBabok/src/main/java/hu/unideb/inf/control/FXMLSearchPageSceneController.java
+++ BarnaBabok/src/main/java/hu/unideb/inf/control/FXMLSearchPageSceneController.java
@@ -653,6 +653,34 @@ public class FXMLSearchPageSceneController implements Initializable {
 
     @FXML
     void handleAddComponentButtonClicked(MouseEvent event) {
+        // Hibakezelések
+        if (ComponentChoiceBox.getSelectionModel().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Kérlek válassz ki egy kilistázandó elemet!");
+            alert.setTitle("Hiba!");
+            alert.setHeaderText(null);
+
+            alert.showAndWait();
+            return;
+        } else if (productTableView.getItems().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Kérlek listázz ki elemeket a hozzáadás előtt!");
+            alert.setTitle("Hiba!");
+            alert.setHeaderText(null);
+
+            alert.showAndWait();
+            return;
+        } else if (productTableView.getSelectionModel().getSelectedItems().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Kérlek jelölj ki egy elemet a hozzáadáshoz!");
+            alert.setTitle("Hiba!");
+            alert.setHeaderText(null);
+
+            alert.showAndWait();
+            return;
+        }
+
+        // egyéb
         String selectedComponent = ComponentChoiceBox.getValue();   // checkbox-ban kiválasztott komponenst jelöli
 
         ObservableList<PCComponentWrapper> items = FXCollections.observableArrayList();
